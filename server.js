9786c32836b4a7a51dcd6df581eff3f2ef708093
unknown <mattcmej@gmail.com>
2018-07-05 20:47:16 -0700

    Change server.js variables to constants.

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 // Our scraping tools
 // I will use 'request' instead of axios.
 
 // var axios = require("axios");
-var request = require("request");
-var cheerio = require("cheerio");
+const request = require("request");
+const cheerio = require("cheerio");
 
 // Require all models
 var db = require("./models");
 
 // var PORT = 3000;
 
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 
 // Initialize Express
-var app = express();
+const app = express();
 
 //set up handlebars
-var exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
@@ -38,7 +38,7 @@ require("./routes")(app);
 // Connect to the Mongo DB
 // mongoose.connect("mongodb://localhost:27017/week18Populater");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
