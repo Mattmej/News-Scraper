88db23e136a7646272a6cc06dcdb903175bb570c
unknown <mattcmej@gmail.com>
2018-07-05 20:44:50 -0700

    Work on fetch.js.

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -1,4 +1,20 @@
+// This controller is in charge of fetching all articles.
+
 var express = require("express");
+var request = require("request");
 
 var router = express.Router();
+// var headline = require("../models/Headline.js");
+// var note = require("../models/Note.js");
+var db = require("./models");
+
+router.get("/all", function(req, res) {
+    db.Headline.find({})
+    .then(function(dbArticle) {
+        res.json(dbArticle);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
 
