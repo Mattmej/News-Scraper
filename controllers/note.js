98971e95c469ff0e808f4ca970530d6cdebd29e7
unknown <mattcmej@gmail.com>
2018-07-07 04:17:12 -0700

    Complete (probably) note.js controller.

--- controllers/note.js
+++ controllers/note.js
@@ -1,23 +1,69 @@
 // This controller is in charge of fetching all articles.
 
-const express = require("express");
+// const express = require("express");
 // var bodyParser = require("body-parser");
-const request = require("request");
+// const request = require("request");
 
-const noteRouter = express.Router();
+// const noteRouter = express.Router();
 const db = require("./models");
 
-noteRouter.post("/articles/:id", function(req, res) {
-    db.Note.create(req.body)
-    .then(function(dbNote) {
-        return db.Headline.findOneAndUpdate({_id: req.params.id}, {note: dbNote._id}, {new: true});  
-    })
-    .then(function(dbArticle) {
-        res.json(dbArticle);
-    })
-    .catch(function(err) {
-        res.json(err);
-    });
-});
+module.exports = {
+    findOneNote: function(req, res) {
+        db.Note
+        .findOne(req.query)
+        .then(function(dbNote) {
+            res.json(dbNote);
+        });
+    },
 
-module.exports = noteRouter;
+    createNote: function(req, res) {
+        db.Note
+        .create(req.body)
+        .then(function(dbNote) {
+            res.json(dbNote);
+        });
+    },
+
+    deleteNote: function(req, res) {
+        db.Note
+        .deleteOne({_id: req.params.id})
+        .then(function(dbNote) {
+            res.json(dbNote);
+        });
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////
+
+
+
+// For 'routes' file?
+
+// noteRouter.post("/articles/:id", function(req, res) {
+//     db.Note.create(req.body)
+//     .then(function(dbNote) {
+//         return db.Headline.findOneAndUpdate({_id: req.params.id}, {note: dbNote._id}, {new: true});  
+//     })
+//     .then(function(dbArticle) {
+//         res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//         res.json(err);
+//     });
+// });
+
+// module.exports = noteRouter;
