8d5f872e443ade9027431682925563edf57584a6
unknown <mattcmej@gmail.com>
2018-07-08 16:59:12 -0700

    Work on controllers/fetch.js.

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -7,6 +7,36 @@ const fetchRouter = express.Router();
 // var headline = require("../models/Headline.js");
 // var note = require("../models/Note.js");
 const db = require("./models");
+const scrape = require("../scripts/scrape");
+
+module.exports = {
+    scrapeHeadlines: function(req, res) {
+        return scrapeArticles()
+        .then(function(articles) {
+
+            // create an item in the Headline schema.
+            return db.Headline.create(articles);
+        })
+        .then(function(dbHeadline) {
+            if (dbHeadline.length === 0) {
+                res.json({
+                    message: "No new articles at this time."
+                });
+            }
+
+            else {
+                res.json({
+                    message: `Added ${dbHeadline.length} new articles.`
+                });
+            }
+        })
+        .catch(function(err) {
+            res.json({
+                message: "Scrape complete."
+            });
+        });
+    }
+};
 
 
 
